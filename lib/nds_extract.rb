03fb19ae805fcc4cef5f3f4af3d6b2038321c49b
Alice Won <alicewon4@gmail.com>
2019-10-30 22:35:35 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -33,5 +33,3 @@ def gross_for_director(directors_database)
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
-def gross_for_director(director_data)
-end
